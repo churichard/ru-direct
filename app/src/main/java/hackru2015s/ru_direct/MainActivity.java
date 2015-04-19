fbdfedc6125cec338b46128051bd9ddc038dba99
Richard Chu <richardchu10@gmail.com>
2015-04-18 22:04:42 -0400

    Merge changes

--- app/src/main/java/hackru2015s/ru_direct/MainActivity.java
+++ app/src/main/java/hackru2015s/ru_direct/MainActivity.java
@@ -1,17 +1,56 @@
 package hackru2015s.ru_direct;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private class GetJsonTask extends AsyncTask<Void, Void, String> {
+        protected String doInBackground(Void... voids) {
+            return getJSON("http://runextbus.herokuapp.com/active");
+        }
+
+        protected void onPostExecute(String result) {
+            Log.d("Bus Json", result);
+            TextView buses = (TextView) findViewById(R.id.bus);
+            buses.setText(result);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new GetJsonTask().execute();
+
+        final Button refresh = (Button) findViewById(R.id.refresh);
+        refresh.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new GetJsonTask().execute();
+            }
+        });
     }
 
     @Override
@@ -34,5 +73,33 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // Gets JSON from an address
+    public String getJSON(String address){
+        StringBuilder builder = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(address);
+        try{
+            HttpResponse response = client.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            if(statusCode == 200){
+                HttpEntity entity = response.getEntity();
+                InputStream content = entity.getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                String line;
+                while((line = reader.readLine()) != null){
+                    builder.append(line);
+                }
+            } else {
+                Log.e(MainActivity.class.toString(), "Failed to get JSON object");
+            }
+        }catch(ClientProtocolException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return builder.toString();
     }
 }
