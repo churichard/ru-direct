725cee2d6c957ed9c747008a2e3f9d9ac9f8aa70
Richard Chu <richardchu10@gmail.com>
2015-06-23 00:05:56 -0400

    Change active bus and current loc icons

--- app/src/main/java/me/rutgersdirect/rudirect/fragment/BusMapFragment.java
+++ app/src/main/java/me/rutgersdirect/rudirect/fragment/BusMapFragment.java
@@ -1,6 +1,5 @@
 package me.rutgersdirect.rudirect.fragment;
 
-import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -11,12 +10,12 @@ import android.view.View;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -32,35 +31,39 @@ import me.rutgersdirect.rudirect.api.NextBusAPI;
 import me.rutgersdirect.rudirect.data.model.BusStop;
 
 public class BusMapFragment extends MapFragment implements
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback {
 
     private static final String TAG = BusMapFragment.class.getSimpleName();
     private static final int REQUEST_CODE = 9000;
-    private static final int CURRENT_LOC_REFRESH_INTERVAL = 5000;
     private static final int ACTIVE_BUS_REFRESH_INTERVAL = 10000;
 
     private GoogleMap mMap;
     private BusStopsActivity busStopsActivity;
     private GoogleApiClient mGoogleApiClient;
-    private LocationRequest mLocationRequest;
     private Handler refreshHandler;
-    private ArrayList<Marker> markers;
-    private Marker currentLocation;
+    private ArrayList<Marker> activeBusMarkers;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        busStopsActivity = (BusStopsActivity) getActivity();
+        MapsInitializer.initialize(busStopsActivity);
+    }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        busStopsActivity = (BusStopsActivity) getActivity();
-        mMap = getMap();
-        markers = new ArrayList<>();
-
-        buildGoogleApiClient();
-        createLocationRequest();
-        drawRoute();
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                getLatLng(busStopsActivity.getLatitudes()[0], busStopsActivity.getLongitudes()[0]), 13.0f));
+        activeBusMarkers = new ArrayList<>();
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        buildGoogleApiClient();
+        getMapAsync(this);
+    }
+
+    // Build the Google API Client
     protected synchronized void buildGoogleApiClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(busStopsActivity)
                 .addConnectionCallbacks(this)
@@ -70,11 +73,16 @@ public class BusMapFragment extends MapFragment implements
     }
 
     @Override
-    public void onConnected(Bundle connectionHint) {
-        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        handleNewLocation(location);
-        startLocationUpdates();
+    public void onMapReady(final GoogleMap map) {
+        mMap = map;
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                getLatLng(busStopsActivity.getLatitudes()[0], busStopsActivity.getLongitudes()[0]), 13.0f));
+        mMap.setMyLocationEnabled(true);
+        drawRoute();
     }
+
+    @Override
+    public void onConnected(Bundle connectionHint) { /* Do nothing */}
 
     @Override
     public void onConnectionSuspended(int i) {
@@ -92,16 +100,9 @@ public class BusMapFragment extends MapFragment implements
     }
 
     @Override
-    public void onLocationChanged(Location location) {
-        currentLocation.remove();
-        handleNewLocation(location);
-    }
-
-    @Override
     public void onPause() {
         super.onPause();
         if (mGoogleApiClient.isConnected()) {
-            stopLocationUpdates();
             mGoogleApiClient.disconnect();
         }
     }
@@ -131,13 +132,6 @@ public class BusMapFragment extends MapFragment implements
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    protected void createLocationRequest() {
-        mLocationRequest = LocationRequest.create()
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                .setInterval(CURRENT_LOC_REFRESH_INTERVAL)
-                .setFastestInterval(CURRENT_LOC_REFRESH_INTERVAL);
     }
 
     // Draws the bus route on the map
@@ -174,32 +168,8 @@ public class BusMapFragment extends MapFragment implements
         }
     }
 
-    // Draws the current location on the map
-    private void handleNewLocation(Location location) {
-        if (location != null) {
-            // Add current location to map
-            MarkerOptions markerOptions = new MarkerOptions()
-                    .position(getLatLng(location))
-                    .title("Your current location")
-                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
-            currentLocation = mMap.addMarker(markerOptions);
-        }
-    }
-
-    private LatLng getLatLng(Location location) {
-        return new LatLng(location.getLatitude(), location.getLongitude());
-    }
-
     private LatLng getLatLng(String latitude, String longitude) {
         return new LatLng(Double.parseDouble(latitude), Double.parseDouble(longitude));
-    }
-
-    protected void startLocationUpdates() {
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-    }
-
-    protected void stopLocationUpdates() {
-        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
     }
 
     // Update active bus locations
@@ -222,8 +192,8 @@ public class BusMapFragment extends MapFragment implements
 
                 if (activeLats != null && activeLons != null) {
                     // Clear map of active bus markers
-                    for (int i = 0; i < markers.size(); i++) {
-                        markers.get(i).remove();
+                    for (int i = 0; i < activeBusMarkers.size(); i++) {
+                        activeBusMarkers.get(i).remove();
                     }
 
                     // Add active bus markers
@@ -231,8 +201,8 @@ public class BusMapFragment extends MapFragment implements
                         MarkerOptions markerOptions = new MarkerOptions()
                                 .position(getLatLng(activeLats.get(i), activeLons.get(i)))
                                 .title("Active Bus")
-                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
-                        markers.add(mMap.addMarker(markerOptions));
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_bus));
+                        activeBusMarkers.add(mMap.addMarker(markerOptions));
                     }
                 }
             }
